2dd52fd6fc1f8de2aabd8b6a180b9a315f492cc3
Bao <000477980@student.vcc.ca>
2024-12-09 17:14:03 -0800

    add weaponDetails Route and Create Page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import { useState } from 'react'
 import {useRoutes} from 'react-router-dom'
-import Navbar from './components/navBar'
+import Navbar from './components/Navbar'
 import WeaponPage from './pages/weapons/WeaponPage'
 import Homepage from './pages/HomePage'
 import './App.css'
 import MainPage from './Pages/MainPage'
+import WeaponDetails from './Pages/weapons/WeaponDetails';
+import CreatePage from './pages/CreatePage'
 
 function App() {  
   const routes = useRoutes([
     {
       path: '/',
-      element: <Homepage></Homepage>
+      element: <Homepage />
     },
     {
       //productdetails Page
       path: '/weapons',
       element: <WeaponPage />
+    },
+    {
+      path: '/weapons/:weaponName',
+      element: <WeaponDetails/>
+    }, 
+    {
+      path: '/create',
+      element: <CreatePage/>
     },
 
   ]);
